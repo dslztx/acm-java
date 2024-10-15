aee15859d1d8c8d2f9023f031a69945e91a9c527
dslztx <dslztx@gmail.com>
2024-10-16 00:13:19 +0800

    use Math.max() function

--- src/main/java/me/dslztx/dp/LCS.java
+++ src/main/java/me/dslztx/dp/LCS.java
@@ -30,11 +30,7 @@ public class LCS {
                 if (sm[i - 1] == sn[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    if (dp[i - 1][j] > dp[i][j - 1]) {
-                        dp[i][j] = dp[i - 1][j];
-                    } else {
-                        dp[i][j] = dp[i][j - 1];
-                    }
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
