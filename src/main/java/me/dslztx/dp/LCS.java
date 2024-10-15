8b860090d364da32055efd648d01117489e0fdd0
dslztx <dslztx@gmail.com>
2024-10-15 23:18:28 +0800

    add LCS and LCSTest

--- src/main/java/me/dslztx/dp/LCS.java
+++ src/main/java/me/dslztx/dp/LCS.java
@@ -1,4 +1,37 @@
 package me.dslztx.dp;
 
 public class LCS {
+
+    char[] sm;
+    char[] sn;
+    int[][] dp;
+
+    public LCS(char[] sm, char[] sn) {
+        this.sm = sm;
+        this.sn = sn;
+    }
+
+    public void buildDP() {
+        int m = sm.length;
+        int n = sn.length;
+        dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (sm[i - 1] == sn[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    if (dp[i - 1][j] > dp[i][j - 1]) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 1];
+                    }
+                }
+            }
+        }
+    }
+
+    public int obtainLCSValue() {
+        return dp[sm.length][sn.length];
+    }
 }
--- src/test/java/me/dslztx/dp/LCSTest.java
+++ src/test/java/me/dslztx/dp/LCSTest.java
@@ -1,7 +1,27 @@
 package me.dslztx.dp;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class LCSTest {
 
+    @Test
+    public void test0() {
+        try {
+            char[] sm = new char[] {'1', '5', '2', '6', '8', '7'};
+            char[] sn = new char[] {'2', '3', '5', '6', '9', '8', '4'};
+
+            LCS lcs = new LCS(sm, sn);
+            lcs.buildDP();
+
+            Assertions.assertTrue(lcs.obtainLCSValue() == 3);
+        } catch (Exception e) {
+            log.error("", e);
+            Assertions.fail();
+        }
+    }
 }
